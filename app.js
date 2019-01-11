a62ebb228767b78344a70b898b9b013480ff960e
Vitaliy Markitanov <VM2213@dhltd.corp>
2019-01-11 10:45:01 -0500

    added req

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 require('dotenv').config();
+const util = require('util');
 
 
 var app = express();
 
-app.get("/",(req,res,next)=>{
-    var env = JSON.stringify(process.env);
-    res.end(env);
+app.get("/", (req, res, next) => {
+  var env = process.env;
+ 
+  res.write(util.inspect({
+    env,
+    req
+  },));
 });
 
-app.listen(process.env.PORT || 100, ()=>{console.log(`Listening on port ${process.env.PORT ||100}`)});
+app.listen(process.env.PORT || 100, () => { console.log(`Listening on port ${process.env.PORT || 100}`) });
