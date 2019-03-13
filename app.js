a804aa64615d204860bb0291f4f9dffbe3598ab7
Vitaliy Markitanov <VM2213@dhltd.corp>
2019-03-12 20:03:36 -0400

    added body parsers

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const util = require('util');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.text());
+app.use(bodyParser.raw())
 
 app.use((req, res, next) => {
   var env = process.env;
