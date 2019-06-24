7bf74765c091ee07c27e683a5bd00c08e3e2ff26
Vitaliy <9357021+vit100@users.noreply.github.com>
2019-06-24 02:15:46 -0400

    resp.send instea of write

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('body-parser-xml')(bodyParser);
-require('dotenv').config();
 const util = require('util');
+
+require('dotenv').config();
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.text());
-app.use(bodyParser.raw())
+app.use(bodyParser.raw());
 app.use(bodyParser.xml());
 
 app.use((req, res, next) => {
-  var env = process.env;
-
-  res.write(util.inspect({
-    req,
-    env,
-  }));
- res.end();
+  res.send(
+    util.inspect({
+      req,
+      env: process.env
+    })
+  );
 });
 
-app.listen(process.env.PORT || 100, () => { console.log(`Listening on port ${process.env.PORT || 100}`) });
+app.listen(process.env.PORT || 100, () => {
+  console.log(`Listening on port ${process.env.PORT || 100}`);
+});
